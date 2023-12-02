6d3ea65a9828c9bcf2b7d4120d1082d655682cc8
Plawro <ales.hanacekit@seznam.cz>
2023-12-02 14:01:38 +0100

    Update index.php

--- SwiftSave/index.php
+++ SwiftSave/index.php
@@ -1,42 +1,27 @@
-
-<!DOCTYPE html> 
-<html> 
- <head> 
-  <title> Ajax JavaScript File Upload Example </title> 
- </head> 
- <body>
-  <!-- HTML5 Input Form Elements -->
-  <input id="fileupload" type="file" name="fileupload" /> 
-  <button id="upload-button" onclick="uploadFile()"> Upload </button>
-
-  <!-- Ajax JavaScript File Upload Logic -->
-  <script>
-  async function uploadFile() {
-  let formData = new FormData(); 
-  formData.append("file", fileupload.files[0]);
-  await fetch('/upload.php', {
-    method: "POST", 
-    body: formData
-  }); 
-  alert('The file has been uploaded successfully.');
-  }
-  </script>
-
-<?php
-
-/* Get the name of the uploaded file */
-$filename = $_FILES['file']['name'];
-
-/* Choose where to save the uploaded file */
-$location = "upload/".$filename;
-
-/* Save the uploaded file to the local filesystem */
-if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
-  echo 'Success'; 
-} else { 
-  echo 'Failure'; 
-}
-
-?>
- </body> 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="upload.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
 </html>
+<?PHP
+  if(!empty($_FILES['uploaded_file']))
+  {
+    $path = "Files/";
+    $path = $path . basename( $_FILES['uploaded_file']['name']);
+
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
+  }
+?>
